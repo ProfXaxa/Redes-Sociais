549b68a58dde43ac1e48d1050e55018d0063fd45
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-09-26 11:19:20 -0300

    await

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -2,7 +2,7 @@ const url ='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-gl
 
 async function visualizarInfomacoesGlobais() {
     const res = await fetch(url)
-    const dados = res.json()
+    const dados = await res.json()
     console.log(dados);    
 }
 visualizarInfomacoesGlobais()
