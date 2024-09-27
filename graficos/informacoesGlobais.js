820ec2dc5e91c6e3c49d19d1a98ec5550ccd64e3
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-09-27 11:53:22 -0300

    paragrafo

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,6 +3,9 @@ const url ='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-gl
 async function visualizarInfomacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundos} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
     console.log(dados);    
 }
 visualizarInfomacoesGlobais()
