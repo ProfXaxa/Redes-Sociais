239e1e26cbb89434e9d8ad61a420f20813baf951
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-09-27 11:57:13 -0300

    paragrafo 3.0

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,7 +5,7 @@ async function visualizarInfomacoesGlobais() {
     const dados = await res.json()
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundos} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
     console.log(paragrafo);    
 }
 visualizarInfomacoesGlobais()
