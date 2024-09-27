ebf9cc7922fee435d45d8a185ca684ae6a925c12
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-09-27 12:04:20 -0300

    paragrafo 4.1

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,6 +6,8 @@ async function visualizarInfomacoesGlobais() {
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
     console.log(paragrafo);    
 }
 visualizarInfomacoesGlobais()
