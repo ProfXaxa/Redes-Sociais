58c66cc5e60a042af3a1457c31423e492ef86584
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-09-30 11:53:59 -0300

    Estilizabdo texto 1.1

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,7 +5,7 @@ async function visualizarInfomacoesGlobais() {
     const dados = await res.json()
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span>de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas} </span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio} </span> horas conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
     console.log(paragrafo);    
