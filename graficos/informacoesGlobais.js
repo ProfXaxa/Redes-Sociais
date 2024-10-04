99b00947e9bebbe1c9966bfd737a929e77ff6fb6
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-10-04 09:18:09 -0300

    horas

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,9 +4,11 @@ async function visualizarInfomacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
+    const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
+    const horas = parseInt(dados.tempo_medio)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas}</span> horas conectadas.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
        
