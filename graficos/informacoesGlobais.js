514c5a5aa6e5d722b6255f8fb9ee38d1285424fb
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-10-04 09:09:53 -0300

    pessoas

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,11 +3,12 @@ const url ='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-gl
 async function visualizarInfomacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
-    console.log(paragrafo);    
+       
 }
 visualizarInfomacoesGlobais()
