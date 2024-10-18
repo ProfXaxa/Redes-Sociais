5ca95efdce32bb06e6ea7f1be9bb28cafd623c2c
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-10-18 09:24:43 -0300

    porcentagem

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -7,9 +7,11 @@ async function visualizarInfomacoesGlobais() {
     const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
     const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio - horas)*100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) *100).toFixed(2)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas}</span> horas e <span>${minutos}</span> minutos conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas}</span> horas e <span>${minutos}</span> minutos conectadas. 
+    <br> Isso significa que aproximadamente  ${porcentagemConectada}% de pessoas estão conectadas em alguma rede social.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
        
