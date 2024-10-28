7c9dceb6225cffe0ee3637126223f0f81e04c2ee
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-10-28 13:33:26 -0300

    teste graficos

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -12,9 +12,9 @@ async function quantidadeUsuariosPorRede() {
         }
     ]
     const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
+        grafico.className = 'grafico'
+        document.getElementById('graficos-container').appendChild(grafico)
+        Plotly.newPlot(grafico, data)
     
 
 
