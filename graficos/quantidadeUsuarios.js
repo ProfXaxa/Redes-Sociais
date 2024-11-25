7994f53b1d57a669dae515dbb39c7e3a645043ea
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-11-25 13:55:25 -0300

    teste gráfico pronto

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -21,20 +21,39 @@ async function quantidadeUsuariosPorRede() {
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
-        title:{
-            Text: 'Redes Sociais com mais usuários no mundo',
-            x:0,
-            font:{ 
-                color: getCSS("--primary-color")                
+        title: {
+            text: 'Redes sociais com mais usuários',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                size: 30,
+                font: getCSS('--font')
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Nome das redes',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
             }
         }
-
     }
 
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, layout)
+    Plotly.newPlot(grafico, data, laytout)
 }
 
 quantidadeUsuariosPorRede()
